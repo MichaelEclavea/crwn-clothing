b55a178dcb452731b3174584470a926f8672d757
Michael Eclavea <usermike@gmail.com>
2021-03-04 18:03:40 -0800

    added header with logo, and option links with styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import HomePage from './pages/homepage/HomePage'
 import ShopPage from './pages/shop/ShopPage'
+import Header from './components/header/Header'
 import './App.css';
 
 function App() {
   return (
     <Router>
+    <Header/>
+    <Switch>
     <Route path='/' exact component={HomePage}/>
     <Route path='/shop' component={ShopPage}/>
+    </Switch>
     </Router>
   );
 }
