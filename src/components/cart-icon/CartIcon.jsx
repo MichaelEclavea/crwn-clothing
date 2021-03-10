fdb620a99329ae9dccdc1fa786c2731588234df7
Michael Eclavea <usermike@gmail.com>
2021-03-10 10:27:55 -0800

    created cart redux compoment that has global state of hidden, placed into cartIcon and header

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { toggleCartHidden } from '../../redux/cart/cartActions'
 import { ReactComponent as ShoppingIcon } from './shopping-bag.svg'
 import './cart.scss'
 
-const CartIcon = () => {
+const CartIcon = ({ toggleCartHidden }) => {
+
     return(
-        <div className='cart-icon'>
+        <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
         <span className='item-count'>0</span>
         </div>
     )
 }
 
-export default CartIcon
+const mapDispatchToProps = dispatch => ({
+    toggleCartHidden: () => dispatch(toggleCartHidden())
+})
+
+export default connect(null, mapDispatchToProps)(CartIcon);
+
+// export default CartIcon
---
+++ src/redux/cart/cartReducer.js
@@ -0,0 +1,16 @@
+import CartActionTypes from './cartActions';
+import {TOGGLE_CART_HIDDEN} from './cartConstants';
+const INITIAL_STATE = { hidden: true };
+
+
+const cartReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type){
+        case TOGGLE_CART_HIDDEN:{
+            return {...state, hidden: !state.hidden}
+        }
+        default: 
+        return state;
+    }
+}
+
+export default cartReducer;
