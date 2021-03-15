41413a9758e0f152af42a2096dacd74d8c7023f7
Michael Eclavea <usermike@gmail.com>
2021-03-15 10:25:47 -0700

    completed shop page, split collection component into smaller component added route in shop page and completed shop reducer, collection reducer

--- src/components/directory/Directory.jsx
+++ src/components/directory/Directory.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import {createStructuredSelector} from 'reselect'
-import {selectDirectorySections} from '../../components/directory/directorySelector'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+import { selectDirectorySections } from '../../redux/directory/directorySelector'
 import MenuItem from '../menu-item/MenuItem'
 import './directory.scss'
 
-const Directory = ({sections}) => {
+const Directory = ({ sections }) => {
   return (
     <div className="directory">
       {sections.map(({title, imageUrl, id, size}) => (
