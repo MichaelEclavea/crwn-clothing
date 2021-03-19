ae3a8e420367c2a3fe39137f66fc03cb2d8bd110
Michael Eclavea <usermike@gmail.com>
2021-03-18 19:03:25 -0700

    function homepage on click to categories and changed cursor only on title

--- src/components/menu-item/MenuItem.jsx
+++ src/components/menu-item/MenuItem.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import './menu-item.scss'
 
 const MenuItem = ({ title, imageUrl, size }) => {
-
+    let history = useHistory();
     return(
         <div className={`${size} menu-item`}>
         <div className='background-image' style={{ backgroundImage: `url(${imageUrl})`}}></div>
-        <div className='content'>
+        <div className='content' onClick={() => history.push(`/shop/${title}`)}>
             <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
